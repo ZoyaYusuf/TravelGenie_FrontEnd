c73fe18e9e52a360d7375cbce3c3bc3d297a7d78
ZoyaYusuf <zoyayusuf04@gmail.com>
2026-07-09 02:07:54 +0530

    new cerf

--- src/Components/HomePage/HeroVideo.jsx
+++ src/Components/HomePage/HeroVideo.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "./HeroVideo.css";
-import Video from "/src/assets/Demo.mp4"
+// import Video from "/src/assets/Demo.mp4";
+import hero from "/src/assets/hero.png";
 
 const HeroVideo = () => {
   return (
     <div className="hero-wrapper">
       <div className="hero-container">
-        <video
+        {/* <video
           src={Video}
           autoPlay
           muted
           loop
           playsInline
           className="hero-video"
-        />
+        /> */}
+        <img src={hero} alt="hero" className="hero-video" />
       </div>
     </div>
   );
